541f2d7c80abaca384d90787590882b5f606e8ce
Ankit Das <ankitsonu240@gmail.com>
2024-06-11 17:20:29 +0530

    completed landing page

---
+++ src/components/LandingPageComponents/Review.jsx
@@ -0,0 +1,27 @@
+import { FaStar } from "react-icons/fa";
+
+const Review = ({ review }) => {
+
+    return (
+        <div className="space-y-4">
+
+            <h1 className='text-lg font-syne font-bold leading-6 tracking-normal'>{review.companyName}</h1>
+
+            <div className="flex justify-start items-center gap-4 ">
+                {
+                    review.rating.map((rating, index) => (
+
+                        <div key={index} className='text-2xl text-green-700'><FaStar /></div>
+
+                    ))
+                }
+
+            </div>
+            
+            <p> Rating : <span className='border-r-[1px] border-black pr-1 font-kanit font-normal leading-6 tracking-normal'>{review.scores.ratingScore}</span> <span className='font-kanit font-normal leading-6 tracking-normal'>{review.scores.reviewsCount}</span> reviews</p>
+            
+        </div>
+    )
+}
+
+export default Review
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,22 @@
 import BannerL from "../components/LandingPageComponents/BannerL"
 import HeroL from "../components/LandingPageComponents/HeroL"
+import Review from "../components/LandingPageComponents/Review"
+import { landingPageReview } from "../constants/landingPageConstant"
 
 const LandingPage = () => {
   return (
-    <div className="lg:w-[900px] xl:w-[1140px] 2xl:w-[1400px] 3xl:w-[1600px] mx-auto">
-    <HeroL />
-    <BannerL />
+    <div className="lg:w-[900px] xl:w-[1140px] 2xl:w-[1400px] 3xl:w-[1600px] mx-auto pt-10">
+      <HeroL />
+      <div className="flex justify-between items-center gap-6 flex-wrap ">
+        {
+          landingPageReview.map((review, index) => (
+
+            <Review key={index} review={review} />
+
+          ))
+        }
+      </div>
+      <BannerL />
     </div>
   )
 }
